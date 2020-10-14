84fe40522ae612587ba531d5f612d28d177b4545
Zachary Durr <zdurr22@gmail.com>
2020-10-14 00:50:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].to_s
+		 programmer_hash[:dennis_ritchie][:languages].strip
 end
 
 def adding_matz
